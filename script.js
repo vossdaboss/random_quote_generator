0f95961d60a9cb151177e3c5102c5c6ac25f2560
Justus Voss <justusvoss27@gmail.com>
2023-02-15 07:32:56 -0800

    Changes to be committed: 	deleted:    brainyqoute_scraper.py 	new file:   quotes.json 	modified:   script.js

--- script.js
+++ script.js
@@ -1,27 +1,20 @@
-const quotes = [
-    {
-      text: "Be the change you wish to see in the world.",
-      author: "Mahatma Gandhi"
-    },
-    {
-      text: "The only way to do great work is to love what you do.",
-      author: "Steve Jobs"
-    },
-    {
-      text: "Believe you can and you're halfway there.",
-      author: "Theodore Roosevelt"
-    }
-  ];
-  
-  const newQuoteButton = document.querySelector("#new-quote-button");
-  const quoteContainer = document.querySelector("#quote-container");
-  
+fetch('quotes.json')
+  .then(response => response.text())
+  .then(data => {
+    const quotes = JSON.parse(data);
+
+
+    const newQuoteButton = document.querySelector("#new-quote-button");
+    const quoteContainer = document.querySelector("#quote-container");
+
   newQuoteButton.addEventListener("click", () => {
     const randomIndex = Math.floor(Math.random() * quotes.length);
     const randomQuote = quotes[randomIndex];
     const quoteHTML = `
-      <p>"${randomQuote.text}"</p>
-      <p>- ${randomQuote.author}</p>
+      <p>"${randomQuote.Quote}"</p>
+      <p>- ${randomQuote.Author}</p>
     `;
     quoteContainer.innerHTML = quoteHTML;
-  });
+  });  
+}); 
+// Path: quotes.json
